fbc67071489240c1e24bc0420ca834ab93b852a5
bravejang <wkdrmfmt@gmail.com>
2021-03-10 08:09:01 +0900

    11강 - 이벤트 처리하기

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,67 +1,29 @@
 import React from 'react';
 import 'App.css';
-import propTypes from "prop-types"
-import ThemedButton from "ThemedButton"
-import {Button} from 'antd';
+import propTypes from "prop-types";
+import Counter from "Counter";
 
-class PostDetail extends React.Component {
-  static propTypes = {
-    postId : propTypes.number.isRequired
-  }
-  state = {
-    postDetail : null,
-  }
-  componentDidMount() {
-    const { postId } = this.props;
-    this.requestPost(postId);
-  }
-
-  componentDidUpdate(prevProps) {
-    const { postId } = this.props;
-    if (prevProps.postId !== postId ) {
-      this.requestPost(postId);    }
-  }
-
-  requestPost(postId) {
-    console.log(`request post #${postId}`);
-    this.setState({
-      postDetail : null
-    });
-    setTimeout(() => {
-      this.setState({
-        postDetail : `로딩된 post #${postId}`
-      })
-    }, 3000);
-  }
-
-  render() {
-    const { postId } = this.props;
-    const { postDetail } = this.state;
-    return (
-      <div>
-        포스팅 #{postId}
-        <hr />
-        { !postDetail && "로딩 중..."}
-        { postDetail }
-      </div>
-    )
-  }
-}
 
 class App extends React.Component {
-  state = {
-    postId : 10
-  }
+  state = { myquery : "", language : "" }
+
+  onChange = (e) => {
+    const { name, value} = e.target;
+    this.setState({
+      [name] : value,
+    });
+  };
+
   render() {
     return(
-      <div>
-        <ThemedButton theme="success" label="Hello"/>
-        <PostDetail />
-        <button onClick={() => this.setState({ postId : 20})}>
-          postId 변경
-        </button>
-      </div>
-    )
+      <>
+        <Counter onClick={() => console.log("clicked")}/>
+        <input name="myquery" onChange={this.onChange} />
+        <input name="language" onChange={this.onChange} /> 
+        <hr />
+        {JSON.stringify(this.state)}
+      </>
+    );
   }
 }
 
