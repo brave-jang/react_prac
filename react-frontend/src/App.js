bc3a02cc6f8832914932c85304555a334f5a98d4
bravejang <wkdrmfmt@gmail.com>
2021-03-10 08:45:22 +0900

    12강 - 첫 리엑트 컴포넌트 만들기(클릭 카운터)

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,24 +5,13 @@ import Counter from "Counter";
 
 
 class App extends React.Component {
-  state = { myquery : "", language : "" }
-
-  onChange = (e) => {
-    const { name, value} = e.target;
-    this.setState({
-      [name] : value,
-    });
-  };
-
   render() {
-    return(
-      <>
-        <Counter onClick={() => console.log("clicked")}/>
-        <input name="myquery" onChange={this.onChange} />
-        <input name="language" onChange={this.onChange} /> 
-        <hr />
-        {JSON.stringify(this.state)}
-      </>
+    return (
+      <div>
+        <Counter />
+        <Counter color="green"/>
+        <Counter color="blue"/>
+      </div>
     );
   }
 }
