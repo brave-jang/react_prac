7b8daaae1f7cc1ffd7eabc6fcf3073c6157eb52c
bravejang <wkdrmfmt@gmail.com>
2021-03-10 09:44:34 +0900

    13 - CSS를 적용하는 다양한 방법

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import 'App.css';
 import propTypes from "prop-types";
 import Counter from "Counter";
+import Message from "Message";
+import Profile from "Profile";
 
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Message />
+        <Profile />
         <Counter />
         <Counter color="green"/>
         <Counter color="blue"/>
